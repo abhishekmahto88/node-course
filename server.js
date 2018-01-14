ff076925adae8bf06680a0a031831e4c9ab7cf4c
abhishekmahto88 <abhishekmahto88@outlook.com>
2018-01-14 19:04:30 +0530

    New project added

--- server.js
+++ server.js
@@ -57,6 +57,13 @@ app.get('/about',(req,res)=>{
   });
 });
 
+app.get('/projects',(req,res)=>{
+  // res.send('About page');
+  res.render('projects.hbs',{
+    pageTitle:'Projects Page',
+  });
+});
+
 app.get('/bad',(req,res)=>{
   res.send({
     errorMessage: 'Unable to handle request'
